04cce426b1c596201c58f0f7bbbde2f755f4c9e9
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-17 20:22:01 -0300

    remove comments

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
-    "@babel/preset-env", // transforma sintaxes do JS para o browser entender. Ex: arrow function, import/export
-    "@babel/preset-react" // transforma sintaxes do REACT para o browser enteder. Ex: JSX
+    "@babel/preset-env", 
+    "@babel/preset-react"
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties"
