01e4d1f59cc5b5417e2728321aedc7e041f6a695
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-17 10:32:27 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // transforma sintaxes do JS para o browser entender. Ex: arrow function, import/export
+    "@babel/preset-react" // transforma sintaxes do REACT para o browser enteder. Ex: JSX
+  ]
+}
